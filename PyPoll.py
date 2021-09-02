2a3ccecdf42a3521e15e31f6eb08cc0276522098
lzhao973 <lzhao973@gmail.com>
2021-09-02 16:14:41 -0700

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -28,22 +28,33 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name]=0
         candidate_votes[candidate_name] += 1
         total_votes += 1
+with open(file_to_save,"w") as txt_file:
+    election_results=(
+        f"\nElection Results\n"
+        f"----------------------\n"
+        f"Total votes:{total_votes:,}\n"
+        f"-----------------------\n")
+    print(election_results,end=" ")
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
         votes=candidate_votes[candidate_name]
         vote_percentage=float(votes)/float(total_votes)*100
-        print(f"{candidate_name}: received {round(vote_percentage,1)}% of the vote")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         if votes>winning_count and vote_percentage>winning_percentage:
-            winning_count=votes
-            winning_percentage=vote_percentage
-            winning_candidate=candidate_name
-        print(f"{winning_candidate},{winning_count},{winning_percentage}")
-    winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+                winning_count=votes
+                winning_percentage=vote_percentage
+                winning_candidate=candidate_name
+        winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+        #print(winning_candidate_summary)
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+    txt_file.write(winning_candidate_summary)
 print(total_votes)
 print(candidate_options)
 print(candidate_votes)
